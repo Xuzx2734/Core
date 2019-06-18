919e6ffa6211d5b0d037a7fccc46f7b5ceb19d54
xuzx <xuzx@fineland.com.cn>
2019-06-18 10:45:24 +0800

    new init

---
+++ src/CoreWeb/ServicesConfig/ServiceExtensions.cs
@@ -0,0 +1,119 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities;
+using CoreWeb.Filter;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repository;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWeb.ServiceConfig
+{
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        ///  Cross-Origin Resource Sharing Configure
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials());
+            });
+        }
+
+        /// <summary>
+        /// IIS mode config
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureIISIntergration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>{});
+        }
+
+        /// <summary>
+        /// connectstr config
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="config"></param>
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config["sqlconnection:connectionstring"];
+
+            //if you want to register multiple context type here , you should use generic context parameter in your ctor.
+            services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContext<CoreContext>(options => options.UseSqlServer(connectionString));
+        }
+
+        /// <summary>
+        ///  logger config
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+
+        /// <summary>
+        /// repo config
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureRepositoryWrapper(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+        }
+
+        /// <summary>
+        /// action filter config
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureActionFilter(this IServiceCollection services)
+        {
+            services.AddScoped<ActionFilter>();
+        }
+
+        /// <summary>
+        /// authorization filter config
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureAuthorizationFilter(this IServiceCollection services)
+        {
+            services.AddScoped<AuthorizationFilter>();
+        }
+
+        public static void ConfigureServicesContainer(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+        }
+
+        /// <summary>
+        /// config automapper mapping relationship
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureAutoMapper(this IServiceCollection services)
+        {
+            var config = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new AutoMapperProfileConfig());
+            });
+
+            var mapper = config.CreateMapper();
+
+            services.AddSingleton(mapper);
+
+        }
+
+    }
+}
---
+++ src/Services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using Contracts;
+using Entities.Models;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Repository;
+
+namespace Services
+{
+    public class UserService : IUserService
+    {
+        private IRepositoryWrapper _repoWrapper;
+        private CoreContext _db;
+
+        public UserService(IRepositoryWrapper repoWrapper, CoreContext db)
+        {
+            _repoWrapper = repoWrapper;
+            _db = db;
+        }
+
+        public FY_User GetUserByAccount(string userAccount)
+        {
+            var query = from x in _db.FY_User.Where(x => x.UserAccount == userAccount)
+                        select x;
+            var user = query.FirstOrDefault();
+            return query.FirstOrDefault();
+        }
+
+        public FY_User GetUserById(Guid userGuid)
+        {
+            var query = from x in _db.FY_User.Where(x => x.UserId == userGuid)
+                        select x;
+
+            var user = query.FirstOrDefault();
+            return user;
+        }
+
+        public IEnumerable<FY_User> GetAllUsers()
+        {
+            var query = from x in _db.FY_User
+                        select x;
+
+            var list = query.ToList();
+
+            return list;
+        }
+
+    }
+}
